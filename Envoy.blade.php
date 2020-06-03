6bf29383d1e5fd461016ea846c8e29166b48ff15
Victor Tolbert <victor.tolbert@gmail.com>
2020-06-03 09:55:11 -0400

    setup work

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,8 +1,8 @@
 @setup
-$repo = 'https://github.com/tolbertdesign/system';
-$branch = $branch ?? 'master';
-$remote = $remote ?? 'gentle-breeze';
-$site = 'tolbert.design';
+$repo = 'https://github.com/tolbertdesign/_server';
+$branch = $branch ?? 'develop';
+$remote = $remote ?? 'floral-morning';
+$site = 'victortolbert.com';
 $release_dir = '/home/forge/releases/' . $site;
 $app_dir = '/home/forge/' . $site;
 $release = 'release_' . date('Y-md-Hi-s');
@@ -15,7 +15,6 @@ function logMessage($message) {
 
 @macro('deploy', ['on' => 'remote'])
 fetch_repo
-checkout_branch
 run_composer
 run_yarn
 generate_assets
@@ -24,29 +23,52 @@ update_symlinks
 @endmacro
 
 @task('fetch_repo')
-{{ logMessage("[0/6] 🚀  Fetching the ".$repo." repository…") }}
-@endtask
-
-@task('checkout_branch')
-{{ logMessage("[1/6] 🌀  Checking out the " . $branch . " branch…") }}
+{{ logMessage("[1/6] 🚀  Fetching the ".$repo." repository…") }}
+[ -d {{ $release_dir }} ] || mkdir -p {{ $release_dir }}
+cd {{ $release_dir }}
+git clone --branch {{ $branch }} {{ $repo }} {{ $release }}
 @endtask
 
 @task('run_composer')
 {{ logMessage("[2/6] 🚚  Running Composer…") }}
+cd {{ $release_dir }}/{{ $release }}
+composer install --prefer-dist --no-scripts;
+php artisan clear-compiled --env=production;
 @endtask
 
 @task('run_yarn', ['on' => 'remote'])
 {{ logMessage("[3/6] 📦  Running Yarn…") }}
+cd {{ $release_dir }}/{{ $release }}
+yarn config set ignore-engines true
+yarn
 @endtask
 
 @task('generate_assets', ['on' => 'remote'])
 {{ logMessage("[4/6] 🌅  Generating assets…") }}
+cd {{ $release_dir }}/{{ $release }}
+yarn run production -- --progress false
 @endtask
 
 @task('update_permissions')
 {{ logMessage("[5/6] 🔑  Updating permissions…") }}
+cd {{ $release_dir }}
+chgrp -R www-data {{ $release }}
+chmod -R ug+rxw {{ $release }}
 @endtask
 
 @task('update_symlinks')
 {{ logMessage("[6/6] 🔗  Updating symlinks…") }}
+ln -nfs {{ $release_dir }}/{{ $release }} {{ $app_dir }}
+chgrp -h www-data {{ $app_dir }}
+
+cd {{ $release_dir }}/{{ $release }};
+ln -nfs ../../.env .env;
+chgrp -h www-data .env;
+
+rm -r {{ $release_dir }}/{{ $release }}/storage/logs;
+cd {{ $release_dir }}/{{ $release }}/storage;
+ln -nfs ../../logs logs;
+chgrp -h www-data logs;
+
+sudo -S service php7.3-fpm reload;
 @endtask
