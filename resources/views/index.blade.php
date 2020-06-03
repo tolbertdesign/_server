d832ae9ae77d95adad9f3dae1f4ab83a4e769570
Victor Tolbert <victor.tolbert@gmail.com>
2020-06-03 15:33:03 -0400

    setup work, continues...

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Playground</title>
     <meta property="og:title" content="Booster Test Title" />
-    <meta property="og:description" content="Booster Test Description" />
+    <meta property="og:description" content="Johns Creek Elementary Fun Run is hosting a two-week fundraiser powered by the Booster team! Instead of selling items, families will gather pledges to help their school, while students experience a world-class character program and participate in the Johns Creek Elementary Fun Run. Most students will complete laps at the big event." />
     <meta property="og:site_name" content="http://victortolbert.com/blah" />
     <meta property="og:url" content="http://victortolbert.com/blah" />
     <meta property="og:image" content="http://cominex.net/assets/booster/img/logo/booster_stacked.png" />
