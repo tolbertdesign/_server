9821bcee5db957ba2216a3246d5351c8f9601337
Victor Tolbert <victor.tolbert@gmail.com>
2020-08-24 16:32:33 -0400

    wip

---
+++ resources/views/components/vcard.blade.php
@@ -0,0 +1,14 @@
+<div id="templateBody" class="bodyContent rounded6">
+    <div id="signupThankYouPage" class="contentBox">
+        <h2>Email Sent</h2>
+        <div>For security, we've sent an email to your inbox that contains a link to update your preferences.</div>
+        <div class="vcard"><span class="org fn">Beyond Code GmbH</span>
+            <div class="adr">
+                <div class="street-address">An der Lingenmühle 10</div><span class="locality">Mönchengladbach</span>
+                <span class="postal-code">41061</span>
+                <div class="country-name">Germany</div>
+            </div><br><a href="https://dev.us16.list-manage.com/vcard?u=2bc94708980de7f76aacce188&amp;id=e1ead2c6d8"
+                class="hcard-download">Add us to your address book</a>
+        </div>
+    </div>
+</div>
